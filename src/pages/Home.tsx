dc52f951556bf98f4a2516bc7fcd32aa5bb995d3
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2022-11-30 20:56:30 +0300

    first

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,15 +3,32 @@ import React, { useEffect, useState } from 'react'
 import InputForm from '../components/InputForm'
 import TodoList from '../components/TodoList';
 
+const url = "https://635182113e9fa1244e60855d.mockapi.io/api/todos";
+
 
 const Home = () => {
+  const [todos, setTodos] = useState<TodoType[]>([])
+
+  const getTodos=async()=>{
+    try {
+      const { data } = await axios.get<TodoType[]>(url);
+      console.log(data);
+      setTodos(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getTodos()
+  }, [])
   
   
 
   return (
     <div className='main'>
       <InputForm  />
-      <TodoList />
+      <TodoList todos={todos} />
     </div>
   )
 }
