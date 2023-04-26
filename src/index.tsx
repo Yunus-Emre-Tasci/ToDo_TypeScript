b8a6114724d2b47a4ee92b26fce3b28d8e57cf85
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-04-26 10:04:08 +0300

    Non-null assertion operator used

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//     <App />
+// );
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root")! // Non-null assertion operator used
 );
-root.render(
-    <App />
-);
+
+root.render(<App />);
+
 
